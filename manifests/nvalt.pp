574395a9a671ff2ce871fa93e3e1ea12c5f3b818
Jonathan Groth <grothja@niaid.nih.gov>
2014-05-08 14:32:41 -0400

    Update nvalt.pp

--- manifests/nvalt.pp
+++ manifests/nvalt.pp
@@ -1,5 +1,6 @@
-class notational_velocity::nvalt inherits notational_velocity {
+class notational_velocity::nvalt {
   Package['nvALT'] {
     source   => 'http://abyss.designheresy.com/nvaltb/nvalt2.2b106.zip'
+    provider => 'compressed_app'
   }
 }
