e8746f89d80c1935422a579186482dfe3d0c789e
David Robbins <robbins.david@gmail.com>
2014-06-05 14:10:54 -0500

    Fix a typo

--- manifests/nvalt.pp
+++ manifests/nvalt.pp
@@ -1,6 +1,6 @@
 class notational_velocity::nvalt {
-  Package['nvALT'] {
-    source   => 'http://abyss.designheresy.com/nvaltb/nvalt2.2b106.zip'
+  package { 'nvALT':
+    source   => 'http://abyss.designheresy.com/nvaltb/nvalt2.2b106.zip',
     provider => 'compressed_app'
   }
 }
